3317b2b7882bce1750a01097b971cd01aae5f0ef
Bleikmann <the_pale_man@protonmail.com>
2022-01-06 03:02:50 +0100

    Test update

--- src/main.cpp
+++ src/main.cpp
@@ -66,49 +66,55 @@ namespace
 		static inline REL::Function<ProcessMessage_t> _ProcessMessage;
 		static inline REL::Offset<Tab*> _savedTabIdx = REL::ID(520167);
 	};
+
+	void InitializeLog()
+	{
+#ifndef NDEBUG
+		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+		auto path = logger::log_directory();
+		if (!path) {
+			util::report_and_fail("Failed to find standard logging directory"sv);
+		}
+
+		*path /= fmt::format("{}.log"sv, Plugin::NAME);
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+#ifndef NDEBUG
+		const auto level = spdlog::level::trace;
+#else
+		const auto level = spdlog::level::info;
+#endif
+
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+		log->set_level(level);
+		log->flush_on(level);
+
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+	}
 }
 
 
-extern "C"
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+
+	v.PluginVersion(Plugin::VERSION);
+	v.PluginName(Plugin::NAME);
+
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+
+	return v;
+}();
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
-	{
-		SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\StayAtSystemPage.log");
-		SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::UseLogStamp(true);
+	InitializeLog();
+	logger::info("{} v{}"sv, Plugin::NAME, Plugin::VERSION.string());
 
-		_MESSAGE("StayAtSystemPage v%s", SYSP_VERSION_VERSTRING);
+	SKSE::Init(a_skse);
 
-		a_info->infoVersion = SKSE::PluginInfo::kVersion;
-		a_info->name = "StayAtSystemPage";
-		a_info->version = SYSP_VERSION_MAJOR;
-
-		if (a_skse->IsEditor()) {
-			_FATALERROR("Loaded in editor, marking as incompatible!");
-			return false;
-		}
-
-		auto ver = a_skse->RuntimeVersion();
-		if (ver <= SKSE::RUNTIME_1_5_39) {
-			_FATALERROR("Unsupported runtime version %s!", ver.GetString().c_str());
-			return false;
-		}
-
-		return true;
-	}
-
-
-	bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-	{
-		_MESSAGE("StayAtSystemPage loaded");
-
-		if (!SKSE::Init(a_skse)) {
-			return false;
-		}
-
-		JournalMenuEx::InstallHooks();
-
-		return true;
-	}
+	return true;
 }
